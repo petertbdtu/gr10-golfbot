c81638e9588c2f25523895e9fe526f80ab53a0e1
fagnig <fagnig@autistklassen.dk>
2019-05-01 11:07:30 +0200

    noget med gui

--- GolfBot/gui/src/gui/application/FXController.java
+++ GolfBot/gui/src/gui/application/FXController.java
@@ -2,9 +2,17 @@ package gui.application;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Queue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+
+import golfbot.server.blackboard.BlackboardListener;
+import golfbot.server.blackboard.BlackboardSample;
 
 //import org.opencv.core.Mat;
 //import org.opencv.imgproc.Imgproc;
@@ -15,17 +23,27 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import lejos.robotics.geometry.Point;
 
-public class FXController {
+public class FXController implements BlackboardListener {
+	
+	List<BlackboardSample> samples = new ArrayList<BlackboardSample>();
+	
+	
+	
 	@FXML
 	private Button btn_test;
 	@FXML
@@ -38,168 +56,14 @@ public class FXController {
 	private Button start_btn;
 	@FXML
 	private ImageView currentFrame;
+	@FXML
+	private ScatterChart<Double, Double> scanMap;
 		
 	
 	private ScheduledExecutorService timer; 
-	//private VideoCapture capture = new VideoCapture();
 	private boolean cameraActive = false;
 	private static int cameraId = 0;
 	
-/*	@FXML
-	protected void startCamera(ActionEvent event) {
-		if (!this.cameraActive)
-		{
-			// start the video capture
-			this.capture.open(cameraId);
-			
-			// is the video stream available?
-			if (this.capture.isOpened())
-			{
-				this.cameraActive = true;
-				
-				// grab a frame every 33 ms (30 frames/sec)
-				Runnable frameGrabber = new Runnable() {
-					
-					@Override
-					public void run()
-					{
-						// effectively grab and process a single frame
-						Mat frame = grabFrame();
-						// convert and show the frame
-						Image imageToShow = SwingFXUtils.toFXImage(matToBufferedImage(frame), null);
-						updateImageView(currentFrame, imageToShow);
-					}
-				};
-				
-				this.timer = Executors.newSingleThreadScheduledExecutor();
-				this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
-				
-				// update the button content
-				this.start_btn.setText("Stop Camera");
-			}
-			else
-			{
-				// log the error
-				System.err.println("Impossible to open the camera connection...");
-			}
-		}
-		else
-		{
-			// the camera is not active at this point
-			this.cameraActive = false;
-			// update again the button content
-			this.start_btn.setText("Start Camera");
-			
-			// stop the timer
-			this.stopAcquisition();
-		}
-}
-	
-	//STOLEN CONTENT https://github.com/opencv-java/getting-started/blob/master/FXHelloCV/src/it/polito/elite/teaching/cv/utils/Utils.java
-	private static BufferedImage matToBufferedImage(Mat original)
-	{
-		// init
-		BufferedImage image = null;
-		int width = original.width(), height = original.height(), channels = original.channels();
-		byte[] sourcePixels = new byte[width * height * channels];
-		original.get(0, 0, sourcePixels);
-		
-		if (original.channels() > 1)
-		{
-			image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
-		}
-		else
-		{
-			image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
-		}
-		final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-		System.arraycopy(sourcePixels, 0, targetPixels, 0, sourcePixels.length);
-		
-		return image;
-}
-// stolen end
-	private Mat grabFrame()
-	{
-		// init everything
-		Mat frame = new Mat();
-		
-		// check if the capture is open
-		if (this.capture.isOpened())
-		{
-			try
-			{
-				// read the current frame
-				this.capture.read(frame);
-				
-				// if the frame is not empty, process it
-				if (!frame.empty())
-				{
-					Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
-				}
-				
-			}
-			catch (Exception e)
-			{
-				// log the error
-				System.err.println("Exception during the image elaboration: " + e);
-			}
-		}
-		
-		return frame;
-		
-		
-	}
-	*/
-	/**
-	 * Stop the acquisition from the camera and release all the resources
-	 */ /*
-	private void stopAcquisition()
-	{
-		if (this.timer!=null && !this.timer.isShutdown())
-		{
-			try
-			{
-				// stop the timer
-				this.timer.shutdown();
-				this.timer.awaitTermination(33, TimeUnit.MILLISECONDS);
-			}
-			catch (InterruptedException e)
-			{
-				// log any exception
-				System.err.println("Exception in stopping the frame capture, trying to release the camera now... " + e);
-			}
-		}
-		
-		if (this.capture.isOpened())
-		{
-			// release the camera
-			this.capture.release();
-		}
-	}
-	
-
-	private void updateImageView(ImageView view, Image image)
-	{
-		onFXThread(view.imageProperty(), image);
-	}
-	*/
-	/**
-	 * On application close, stop the acquisition from the camera
-	 *//*
-	protected void setClosed()
-	{
-		this.stopAcquisition();
-}
-	
-	public static <T> void onFXThread(final ObjectProperty<T> property, final T value)
-	{
-		Platform.runLater(() -> {
-			property.set(value);
-		});
-	}
-
-	
-	*/
 	@FXML
 	protected void OnClickBtnTest(ActionEvent event) {
 		System.out.println("XD");
@@ -230,5 +94,65 @@ public class FXController {
            }); 
 	}
 
+	public void startRenderThread() {
+	    Task <Void> task = new Task<Void>() {
+	        @Override public Void call() throws InterruptedException {
+	        	int i = 0;
+		        while(true) {
+		          // some actions
+		          Thread.sleep(500);
+		          i++;
+		          updateMessage("Bitch "+i);
+	
+		        }
+	        }
+	      };
+	      
+	      start_btn.textProperty().bind(task.messageProperty());
+	      
+	      Thread thread = new Thread(task);
+	      thread.setDaemon(true);
+	      thread.start();
+	}
+	
 
+	@Override
+	public void blackboardUpdated(BlackboardSample bbSample) {
+		samples.add(bbSample);
+		
+		ObservableList<String> tmp = list_test.getItems();
+		
+		tmp.add("" + (tmp.size()+1));
+		   
+        list_test.setItems(tmp);
+	}
+	
+	public void init() {
+	    list_test.valueProperty().addListener(new ChangeListener<String>() {
+	        @Override public void changed(ObservableValue ov, String val, String index) {
+	        	BlackboardSample temp = samples.get(Integer.parseInt(index));
+	        	
+	        	
+	        	scanMap.getData().clear();
+
+	        	XYChart.Series<Double, Double> series = null;
+	        	
+	        	for (Entry<Double, Double> entry : temp.scan.entrySet()) {
+	    		    
+	    		    
+	    		    double xx = entry.getKey()*(float)Math.cos(Math.toRadians(entry.getValue()));
+	    		    double yy = entry.getKey()*(float)Math.sin(Math.toRadians(entry.getValue()));
+	    		    
+	    		    series = new Series<Double, Double>();
+	    		    
+	    		    series.getData().add(new XYChart.Data<Double, Double>(xx,yy));
+	    		}
+	        
+	        	if(series != null) {
+	        		scanMap.getData().addAll(series);
+	        	}
+	        	
+	        }    
+	    });
+	}
 }
--- GolfBot/gui/src/gui/application/Main.java
+++ GolfBot/gui/src/gui/application/Main.java
@@ -1,5 +1,8 @@
 package gui.application;
 
+import golfbot.server.blackboard.BlackboardListener;
+import golfbot.server.blackboard.BlackboardSample;
+
 //import org.opencv.core.Core;
 
 import javafx.application.Application;
@@ -12,18 +15,24 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
+	Scene scene;
+	
+	FXController controller;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("Stage.fxml"));
 			AnchorPane root = (AnchorPane) loader.load();
-			FXController controller = loader.getController();
+			controller = loader.getController();
 			
-			Scene scene = new Scene(root,1200,700);
+			scene = new Scene(root,1200,700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
+			controller.init();
+			controller.startRenderThread();
 
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -35,4 +44,5 @@ public class Main extends Application {
 		//System.loadLibrary(Core.NATIVE_LIBRARY_NAME);  
 		launch(args);
 	}
+	
 }
