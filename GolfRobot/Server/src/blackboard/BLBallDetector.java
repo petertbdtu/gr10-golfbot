6ff7689cc33d2787c7627aba5ce604392d1662c5
petertbdtu <s175210@student.dtu.dk>
2019-06-12 10:19:39 +0200

    Make BLBallDetector a BlackboardListener

--- GolfRobot/Server/src/blackboard/BLBallDetector.java
+++ GolfRobot/Server/src/blackboard/BLBallDetector.java
@@ -1,80 +1,50 @@
 package blackboard;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import mapping.LidarScan;
 import objects.LidarSample;
 import objects.Point;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import org.opencv.core.CvType;
-import org.opencv.core.Rect;
 
-public class BLBallDetector {
+public class BLBallDetector extends Thread implements BlackboardListener {
+	
+	private BlackboardSample bbSample;
+	private Point closestBall;
+	
+	public Point getClosestBall() {
+		return closestBall;
+	}
+	
+	@Override
+	public void blackboardUpdated(BlackboardSample bbSample) {
+		this.bbSample = bbSample;
+	}
 
-	public static void main(String args[])
-	{
-		try {
-			BLBallDetector bd = new BLBallDetector();
-
-			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-			FileInputStream fis = new FileInputStream("testScan4.data");
-			ObjectInputStream ois = new ObjectInputStream(fis);
-
-			LidarScan scan = (LidarScan) ois.readObject();
-			ois.close();
-
-			// graphheight in getGraph sometimes crashes if too low for data.
-			//Mat graph = bd.getGraph(scan, 1);
-			//Imgcodecs.imwrite("graph.png", graph);
-			
-			Mat map = bd.getMap(scan);
-			Imgcodecs.imwrite("map.png", map);
-
-			Point closestball = bd.findClosestBallLidar(scan);
-			if (closestball == null) {
-				System.out.println("No balls found.");
-			} else {
-				System.out.println("Closest ball found at "+closestball.toString());
+	@Override
+	public void run() {		
+		int cycle = -1;
+		while(true) {
+			if(bbSample != null) {
+				if(bbSample.cycle == cycle + 1) {
+					closestBall = findClosestBallLidar(bbSample.scan);
+					cycle++;
+				}
 			}
-
-			//System.out.println(bd.scanCameraImage(CAMERA PHOTO));
-
-		}
-		catch (ClassNotFoundException | IOException e) {
-			System.out.println("Could not read test scan data file");
 		}
 	}
 
-	public static Point findCenter(Point a, Point b, Point c)
-	{
-		float k1 = (a.y - b.y) / (a.x - b.x); //Two-point slope equation
-		float k2 = (a.y - c.y) / (a.x - c.x); //Same for the (A,C) pair
-		Point midAB = new Point((a.x + b.x) / 2, (a.y + b.y) / 2); //Midpoint formula
-		Point midAC = new Point((a.x + c.x) / 2, (a.y + c.y) / 2); //Same for the (A,C) pair
-		k1 = -1*k1; //If two lines are perpendicular, then the product of their slopes is -1.
-		k2 = -1*k2; //Same for the other slope
-		float n1 = midAB.y - k1*midAB.x; //Determining the n element
-		float n2 = midAC.y - k2*midAC.y; //Same for (A,C) pair
-		//Solve y1=y2 for y1=k1*x1 + n1 and y2=k2*x2 + n2
-		float x = (n2-n1) / (k1-k2);
-		float y = k1*x + n1;
-		return new Point(0,0); //return new Point(x, y);
-	}
-
-	public Mat getMap(LidarScan scan) {
+	private Mat scanToMap(LidarScan scan) {
 		int lx = 0;
 		int hx = 0;
 		int ly = 0;
@@ -101,7 +71,6 @@ public class BLBallDetector {
 		
 		Mat mat = Mat.zeros(h*2+1, w*2+1, CvType.CV_8U);
 		
-		
 		int lineThickness = 2;
 		if (pts.size() >= 1) {
 			Point prevPoint = pts.get(0);
@@ -119,10 +88,6 @@ public class BLBallDetector {
 			}
 		}
 		
-		// (0, 0) cross which points at 0 degrees (to the right).
-		//Imgproc.line(mat, new org.opencv.core.Point(w-10, h), new org.opencv.core.Point(w+30, h), new Scalar(127), 3, Imgproc.LINE_AA, 0);
-		//Imgproc.line(mat, new org.opencv.core.Point(w, h-10), new org.opencv.core.Point(w, h+10), new Scalar(127), 3, Imgproc.LINE_AA, 0);
-		
 		return mat;
 	}
 	
@@ -132,7 +97,8 @@ public class BLBallDetector {
 	 * @param pixelDistPerDeg the horizontal resolution of the image
 	 * @return the graph
 	 */
-	public Mat getGraph(LidarScan scan, double pixelDistPerDeg) {
+	@SuppressWarnings("unused")
+	private Mat getGraph(LidarScan scan, double pixelDistPerDeg) {
 		int graphheight = 10000;
 		Mat graph = Mat.zeros(graphheight, (int) (360*pixelDistPerDeg), CvType.CV_8U);
 		
@@ -149,9 +115,9 @@ public class BLBallDetector {
 	 * @param scan
 	 * @return the location of the closest ball relative to the lidar
 	 */
-	public Point findClosestBallLidar(LidarScan scan) {
+	private Point findClosestBallLidar(LidarScan scan) {
 		try {
-			Mat map = getMap(scan);
+			Mat map = scanToMap(scan);
 			
 			// Convert to binary image
 			int thresh = 200;
@@ -212,10 +178,12 @@ public class BLBallDetector {
 	
 	/**
 	 * Scans a picture for orange balls
-	 * @param frame picture to search in
+	 * @param image picture to search in
 	 * @return whether there is a ball in the picture
 	 */
-	public boolean scanCameraImage(Mat frame) {	
+	public boolean detectBallInImage(BufferedImage image) {
+		Mat frame = bufferedImageToMat(image);
+		
 		int erosion_value = 0; 
 		int dialation_value = 0;
 		
@@ -257,7 +225,7 @@ public class BLBallDetector {
 		return contours.size() != 0;
 	}
 
-	public static Mat bufferedImageToMat(BufferedImage bi) {
+	private static Mat bufferedImageToMat(BufferedImage bi) {
 		Mat mat = new Mat(bi.getHeight(), bi.getWidth(), CvType.CV_8UC3);
 		byte[] data = ((DataBufferByte) bi.getRaster().getDataBuffer()).getData();
 		mat.put(0, 0, data);
